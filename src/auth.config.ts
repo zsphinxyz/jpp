4685b8615bff829c953e97bff1526b5f4862567d
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2024-05-06 11:29:44 +0630

    original version

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,25 +2,25 @@ import type { NextAuthConfig } from "next-auth";
 import Google from "@auth/core/providers/google"
 
 export default {
-  // providers: [
-  //   Google({
-  //     clientId: process.env.GOOGLE_CLIENT_ID!,
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  //     allowDangerousEmailAccountLinking: true,
-  //     authorization: {
-  //       params: {
-  //         prompt: "consent",
-  //         access_type: "offline",
-  //         response_type: "code"
-  //       }
-  //     },
-  //     profile(profile) {
-  //       console.log("Profile >> ",profile)
-  //       return profile
-  //     }
-  //   }),
-  // ],
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      },
+      profile(profile) {
+        console.log("Profile >> ",profile)
+        return profile
+      }
+    }),
+  ],
+  // providers: [Google],
   session: {strategy: 'jwt'},
   trustHost: true,
 
