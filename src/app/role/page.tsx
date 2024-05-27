2971199695f789f6f00109740fd503bf36a6a001
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2024-05-27 12:06:29 +0630

    role page update

--- src/app/role/page.tsx
+++ src/app/role/page.tsx
@@ -1,36 +1,36 @@
-'use client'
+
 import { Button } from "@/components/ui/button"
-import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { doc, setDoc } from 'firebase/firestore'
 import { db } from "@/lib/firebase";
-import { useRouter } from "next/navigation";
-
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 
 async function Role() {
   // const session = await auth();
   // const user = session?.user;
 
-  const session = useSession()
-  const user = session.data?.user
-  const router = useRouter();
+  const session = await auth()
+  const user = session?.user
   const userID = user?.id!
 
   if(user?.role != 'user') {
     user?.role == 'candidate' ? 
-    router.push('/profile') :
-    router.push('/companyProfile')
+    redirect('/profile') :
+    redirect('/companyProfile')
   }
 
 
   async function CandidateRole() {
+    'use server'
     await setDoc(doc(db, 'users', userID), { role: 'candidate' }, { merge: true })
 
     await setDoc(doc(db, 'profile', userID), { name: user.name, email: user.email }, { merge: true });
   }
 
   async function EmployerRole() {
+    'use server'
     await setDoc(doc(db, 'users', userID), { role: 'employer' }, { merge: true })
 
     await setDoc(doc(db, 'profile', userID), { name: user.name, email: user.email }, { merge: true });
@@ -40,8 +40,15 @@ async function Role() {
     <div className="flex items-center justify-center flex-col w-full h-dvh gap-1">
       <h1 className="text-3xl mb-3">I am a</h1>
       <div className="space-x-3">
-        <Button size="lg" onClick={CandidateRole}> <Link href={'/edit/candidate'}> Candidate </Link></Button>
-        <Button size="lg" onClick={EmployerRole}> <Link href={'/edit/employer'}> Employer </Link></Button>
+
+        <form action={CandidateRole} className="inline">
+          <Button size="lg" type="submit" onSubmit={CandidateRole}> <Link href={'/edit/candidate'}> Candidate </Link></Button>
+        </form>
+
+        <form action={EmployerRole} className="inline">
+          <Button size="lg" type="submit" onSubmit={EmployerRole}> <Link href={'/edit/employer'}> Employer </Link></Button>
+        </form>
+        
       </div>
     </div>
   )
